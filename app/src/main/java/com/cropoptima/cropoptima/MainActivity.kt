4f0596c1ca50809b8f817541522c80e0d750de13
Pandu Wijaya <wijayapandu388@gmail.com>
2023-11-14 20:48:01 +0700

    change parameter from long to long2

--- app/src/main/java/com/cropoptima/cropoptima/MainActivity.kt
+++ app/src/main/java/com/cropoptima/cropoptima/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun test (long: Int): Int{
-        return long
+    fun test (long2: Int): Int{
+        return long2
     }
 }
