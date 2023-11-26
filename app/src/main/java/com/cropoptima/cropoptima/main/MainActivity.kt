df11a9c5302e3abbb6c0f401f6399d729007c48f
Pandu Wijaya <wijayapandu388@gmail.com>
2023-11-26 20:35:48 +0700

    initial auth package

--- app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
+++ app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cropoptima.cropoptima
+package com.cropoptima.cropoptima.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cropoptima.cropoptima.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
