b2d6c536c34fed2774bee8cf7257439654dddd89
faprikaa <muammarm28@gmail.com>
2023-12-12 18:20:47 +0700

    ADD : Login function Register function Logout Function Check if current user is loggedin or not Set identity at profile as current user
    UPDATE:
    Improvement at a few xml

--- app/src/main/java/com/cropoptima/cropoptima/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/cropoptima/cropoptima/auth/register/RegisterFragment.kt
@@ -1,19 +1,22 @@
 package com.cropoptima.cropoptima.auth.register
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.cropoptima.cropoptima.R
 import com.cropoptima.cropoptima.databinding.FragmentRegisterBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.userProfileChangeRequest
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,7 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
 
         return binding.root
     }
@@ -28,9 +32,32 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnRegister.setOnClickListener {
-            Snackbar.make(binding.root, "Register Successful", Snackbar.LENGTH_LONG).show()
+            register()
+//            Snackbar.make(binding.root, "Register Successful", Snackbar.LENGTH_LONG).show()
         }
 
+
+    }
+
+    fun register() {
+        val email = binding.edEmail.text.toString()
+        val password = binding.edPassword.text.toString()
+        val name = binding.edName.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Snackbar.make(binding.root, "Register Successful", Snackbar.LENGTH_LONG).show()
+                val firebaseUser = auth.currentUser!!
+                val profileUpdates = userProfileChangeRequest {
+                    displayName = name
+                    photoUri = Uri.parse("https://picsum.photos/800")
+                }
+
+                firebaseUser.updateProfile(profileUpdates)
+            }
+        }.addOnFailureListener { exception ->
+            Toast.makeText(binding.root.context, exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
+++ app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.cropoptima.cropoptima.main
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cropoptima.cropoptima.R
+import com.cropoptima.cropoptima.auth.AuthActivity
 import com.cropoptima.cropoptima.databinding.ActivityMainBinding
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.userProfileChangeRequest
 
 class
 MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,6 +42,13 @@ MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val user = Firebase.auth.currentUser
+        Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+
+        if (user == null) {
+            startActivity(Intent(binding.root.context, AuthActivity::class.java))
+        }
     }
 
 
