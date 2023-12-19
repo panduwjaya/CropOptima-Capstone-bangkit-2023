f8e042c82aa69da7928f5969b3c614a66dfbc9c4
Pandu Wijaya <wijayapandu388@gmail.com>
2023-12-20 01:20:15 +0700

    Add ResultActivity,ApiService and ApiConfig

---
+++ app/src/main/java/com/cropoptima/cropoptima/data/network/config/ApiService.kt
@@ -0,0 +1,13 @@
+package com.cropoptima.cropoptima.data.network.config
+
+import com.cropoptima.cropoptima.data.network.response.HistoryResponse
+import com.cropoptima.cropoptima.data.network.response.PredictResponse
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("history?")
+    suspend fun postHistory(): HistoryResponse
+
+    @POST("register")
+    suspend fun postPredict(): PredictResponse
+}
---
+++ app/src/main/java/com/cropoptima/cropoptima/main/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.cropoptima.cropoptima.main.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cropoptima.cropoptima.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
