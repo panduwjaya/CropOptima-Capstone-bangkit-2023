a611dd9bb7c8c0bdb03287a7bfce9c9451a85e34
Pandu Wijaya <wijayapandu388@gmail.com>
2023-12-21 14:15:00 +0700

    Fill ApiService

--- app/src/main/java/com/cropoptima/cropoptima/data/network/config/ApiService.kt
+++ app/src/main/java/com/cropoptima/cropoptima/data/network/config/ApiService.kt
@@ -2,16 +2,25 @@ package com.cropoptima.cropoptima.data.network.config
 
 import com.cropoptima.cropoptima.data.network.response.HistoryResponse
 import com.cropoptima.cropoptima.data.network.response.PredictResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiService {
+    @FormUrlEncoded
     @POST("history?")
     suspend fun postHistory(
-
+        @Field("idToken") idToken: String,
     ): HistoryResponse
 
     @POST("register")
     suspend fun postPredict(
-
+        @Field("idToken") idToken: String,
+        @Field("n") n: Float,
+        @Field("p") p: Float,
+        @Field("k") k: Float,
+        @Field("password") ph: Float,
+        @Field("password") lat: Float,
+        @Field("password") lon: Float,
     ): PredictResponse
 }
---
+++ app/src/main/java/com/cropoptima/cropoptima/hilt/AppModule.kt
@@ -0,0 +1,4 @@
+package com.cropoptima.cropoptima.hilt
+
+object AppModule {
+}
